e4d3fb6eebeb982d5512e85662d563a0166a826e
Ibtisam Ur Rehman <ibtisam.rehman321@gmail.com>
2026-05-06 23:28:31 +0500

    implement file reading and pattern matching in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-  cout << "rawgrep v0.0.0";
+  ifstream file("src/main.cpp");
+  string line;
+  string pattern = "include";
+
+  if (!file)
+  {
+    cerr << "Unable to open file\n";
+    return 2;
+  }
+
+  while (getline(file, line))
+  {
+    if (line.find(pattern) != string::npos)
+    {
+      cout << line << "\n";
+    }
+  }
   return 0;
 }
